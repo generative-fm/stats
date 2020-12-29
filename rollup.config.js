7747b3b57070f1542cd2904bd0babc6bb0a640f1
metalex9 <alexbainter@gmail.com>
2020-12-29 15:16:11 -0600

    Add cjs export

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,16 @@ import babel from '@rollup/plugin-babel';
 
 const config = {
   input: 'src/index.js',
-  output: {
-    format: 'esm',
-    file: 'dist/stats.esm.js',
-  },
+  output: [
+    {
+      format: 'esm',
+      file: 'dist/stats.esm.js',
+    },
+    {
+      format: 'cjs',
+      file: 'dist/stats.cjs.js',
+    },
+  ],
   plugins: [babel({ babelHelpers: 'runtime' })],
   external: [/@babel\/runtime/, 'uuid', '@alexbainter/indexed-db'],
 };
